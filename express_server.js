ad834b67401dba7b5fd90690b0b0d5cb20759f5f
ladan <pishili@users.noreply.github.com>
2020-01-21 00:50:10 -0800

    added the key-value functionality for /urls

--- express_server.js
+++ express_server.js
@@ -1,19 +1,47 @@
 // Implementation of a basic web server
 // using the Express.jsframework.
-
+const { generate } = require('./functions');
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
+
+// the body-parser convert the request body
+// from a buffer into string that we can read.  
+// it will then add the data to the req (request) object
+// under the key body
+
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
-// a is not accessible in the other function/callback.
-// The user will NOT see a set to 1 in /fetch. 
-//a is not defined in this scope, and will result
-// in a reference error when anyone visits that URL.
+app.use(bodyParser.urlencoded({extended: true}));
+
+// Sets
+
+app.set("view engine", "ejs");
+
+
+// Gets
+
+app.get("/urls/new", (req, res) => {
+    res.render("urls_new");
+  });
+
+app.get("/urls/:shortURL", (req, res) => {
+    let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+    res.render("urls_show", templateVars);
+  });
+
+app.get("/urls", (req, res) => {
+    // how this route handler will look like
+    let templateVars = { urls: urlDatabase };
+    res.render("urls_index", templateVars);
+  });
+  
 
 app.get("/set", (req, res) => {
     const a = 1;
@@ -26,7 +54,12 @@ app.get("/set", (req, res) => {
 
 
 app.get("/hello", (req, res) => {
-    res.send("<html><body>Hello <b>World</b></body></html>\n");
+    // templateVars object contains string hello world under
+    // key = greeting
+    // pass templateVars object to the template called hello_world
+    let templateVars = { greeting: 'Hello World! ' };
+    res.render("hello_world", templateVars);
+    // res.send("<html><body>Hello <b>World</b></body></html>\n");
   });
 
 
@@ -40,6 +73,19 @@ app.get("/", (req, res) => {
 });
 
 
+// POST
+app.post("/urls", (req, res) => {
+    console.log(req.body);  // Log the POST request body to the console
+    const randomString = generate();
+    //Update your express server so that the shortURL-longURL key-value pair
+    // are saved to the urlDatabase when it receives a POST request to /urls
+    let templateVars = { shortURL: randomString, longURL: urlDatabase[randomString] };
+    res.send(templateVars);         // Respond with 'Ok (we will replace this)
+    // res.render("urls_show", templateVars);
+})
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
+
+
