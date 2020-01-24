722fb580c323f0e454fc3bf4b32335bcb3e24e9a
pishili <pishili@users.noreply.github.com>
2020-01-23 22:31:40 -0800

    updated README.md

--- express_server.js
+++ express_server.js
@@ -96,11 +96,6 @@ app.get("/urls", (req, res) => {
   }
 });
 
-app.get("/urls/:id", (req, res) => {
-  let templateVars = {}
-  res.render("urls_index", templateVars);
-});
-
 app.get("/set", (req, res) => {
   const a = 1;
   res.send(`a = ${a}`);
